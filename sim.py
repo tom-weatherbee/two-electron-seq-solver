ad6dc2c2bfbaa814e84667fc618a2a04c071c429
Hunter Brodie <hunterbrodie@gmail.com>
2024-04-14 17:38:27 -0400

    incorporated neville into optimized file

--- sim.py
+++ sim.py
@@ -17,14 +17,14 @@ def calc_x_super(i, h, size):
     x2 = i // size + 1
     return 1 / max(x1, x2)**2 / h**2
 
-def gs_eigensystem_r(r, size, eigvals_only=False):
+def gs_eigensystem_r(r, size, z, eigvals_only=False):
     h = r / size
 
     I = np.identity(size)
     second = (np.diag(-2 * np.ones(size)) + np.diag(np.ones(size - 1), k=1) + np.diag(np.ones(size - 1), k=-1)) / h**2
     r = np.diag(np.array([calc_r(xi, h) for xi in range(size)]))
     r_super = np.diag(np.array([calc_r_super(xi, h, size) for xi in range(size**2)]))
-    single = -second / 2 - 2 * r
+    single = -second / 2 - z * r
 
     hamiltonian = np.kron(single, I) + np.kron(I, single) + r_super
 
@@ -38,14 +38,14 @@ def gs_eigensystem_r(r, size, eigvals_only=False):
         return (gs, gs_vec)
 
 
-def gs_eigensystem_x(r, size, eigvals_only=False):
+def gs_eigensystem_x(r, size, z, eigvals_only=False):
     h = r / size
 
     I = np.identity(size)
     second = (np.diag(-2 * np.ones(size)) + np.diag(np.ones(size - 1), k=1) + np.diag(np.ones(size - 1), k=-1)) / h**2
     x2 = np.diag(np.array([calc_x(xi, h) for xi in range(size)]))
     x2_super = np.diag(np.array([calc_x_super(xi, h, size) for xi in range(size**2)]))
-    single = (-np.matmul(x2, second) / 8) + (3 / 32 * np.matmul(x2, x2)) - (2 * x2)
+    single = (-np.matmul(x2, second) / 8) + (3 / 32 * np.matmul(x2, x2)) - (z * x2)
 
     hamiltonian = np.kron(single, I) + np.kron(I, single) + x2_super
 
@@ -58,29 +58,12 @@ def gs_eigensystem_x(r, size, eigvals_only=False):
         gs_vec = eigensystem.eigenvectors[:,gs_index]
         return (gs, gs_vec)
 
-neville = []
-first_order = []
-for i in range(9):
-    r = 9
-    size = 20 + i * 5
-    gs = gs_eigensystem_r(r, size, True)
-    first_order.append((r / size, gs))
+def nevilleAlgo(i,j,z):
+    if i == j:
+        return gs_eigensystem_r(9, (i + 4) * 5, 2, True)
+    else:
+        hi2 = (9 / (i + 4) / 5)**2
+        hj2 = (9 / (j + 4) / 5)**2
+        return (nevilleAlgo(i, j - 1, z) * hj2 - hi2 * nevilleAlgo(i + 1, j, z)) / (hj2 - hi2)
 
-neville.append(first_order)
-
-for i in range(8):
-    second_order = []
-    first_order = neville[i]
-    for j in range(8 - i):
-        x0 = first_order[j][0]
-        E0 = first_order[j][1]
-        x1 = first_order[j + 1][0]
-        E1 = first_order[j + 1][1]
-        x = (x0 + x1) / 4
-        value = ((x - x0) * E1 - (x - x1) * E0) / (x1 - x0)
-        second_order.append((x0, value))
-    neville.append(second_order)
-
-
-
-print(neville[len(neville) - 1])
+print(nevilleAlgo(0, 4, 2))
