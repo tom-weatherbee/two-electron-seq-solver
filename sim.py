d2dc55387d1c4b69d3d4a877e5955623b7079d91
Hunter Brodie <hunterbrodie@gmail.com>
2024-04-14 19:45:47 -0400

    idek

--- sim.py
+++ sim.py
@@ -1,8 +1,14 @@
 import numpy as np
 from scipy.linalg import eigh
+from scipy.linalg import eig_banded
+from scipy.linalg import eigh_tridiagonal
+from scipy.linalg.lapack import dsbev
+from scipy.linalg.lapack import ssytrd
 
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
+
+import time
 
 def calc_r(i, h):
     return 1 / (i + 1) / h
@@ -20,7 +26,7 @@ def calc_x_super(i, h, size):
     x2 = i // size + 1
     return 1 / max(x1, x2)**2 / h**2
 
-def gs_eigensystem_r(r, size, z, eigvals_only=False):
+def eigensystem_r(r, size, z, excitation, eigvals_only=False):
     h = r / size
 
     I = np.identity(size)
@@ -32,16 +38,45 @@ def gs_eigensystem_r(r, size, z, eigvals_only=False):
     hamiltonian = np.kron(single, I) + np.kron(I, single) + r_super
 
     if eigvals_only:
-        return min(eigh(hamiltonian, eigvals_only=True))
+        return np.partition(eigh(hamiltonian, eigvals_only=True), excitation)[excitation]
     else:
         eigenvalues, eigenvectors = eigh(hamiltonian)
-        gs = min(eigenvalues)
+        gs = np.partition(eigenvalues, excitation)[excitation]
         gs_index = np.where(eigenvalues == gs)[0][0]
         gs_vec = eigenvectors[:,gs_index]
         return (gs, gs_vec)
 
+def eigensystem_test(r, size, z, excitation, eigvals_only=False):
+    h = r / size
 
-def gs_eigensystem_x(r, size, z, eigvals_only=False):
+    I = np.identity(size)
+    second = (np.diag(-2 * np.ones(size)) + np.diag(np.ones(size - 1), k=1) + np.diag(np.ones(size - 1), k=-1)) / h**2
+    r = np.diag(np.array([calc_r(xi, h) for xi in range(size)]))
+    r_super = np.diag(np.array([calc_r_super(xi, h, size) for xi in range(size**2)]))
+    single = -second / 2 - z * r
+
+    hamiltonian = np.kron(single, I) + np.kron(I, single) + r_super
+
+    t0 = time.time()
+    eig_banded(hamiltonian, eigvals_only=True)
+    print("Banded: " + str(time.time() - t0))
+    
+    t0 = time.time()
+    eigh(hamiltonian, eigvals_only=True)
+    print("Hermitian: " + str(time.time() - t0))
+    
+    t0 = time.time()
+    dsbev(hamiltonian, compute_v=0)
+    print("dsbev: " + str(time.time() - t0))
+    
+    t0 = time.time()
+    tri = ssytrd(hamiltonian)
+    eigh_tridiagonal(tri)
+    print("dsbev: " + str(time.time() - t0))
+    
+eigensystem_test(9, 30, 2, 0)
+
+def eigensystem_x(r, size, z, excitation, eigvals_only=False):
     h = r / size
 
     I = np.identity(size)
@@ -53,29 +88,29 @@ def gs_eigensystem_x(r, size, z, eigvals_only=False):
     hamiltonian = np.kron(single, I) + np.kron(I, single) + x2_super
 
     if eigvals_only:
-        return min(eigh(hamiltonian, eigvals_only=True))
+        return np.partition(eigh(hamiltonian, eigvals_only=True), excitation)[excitation]
     else:
         eigenvalues, eigenvectors = eigh(hamiltonian)
-        gs = min(eigenvalues)
+        gs = np.partition(eigenvalues, excitation)[excitation]
         gs_index = np.where(eigenvalues == gs)[0][0]
         gs_vec = eigenvectors[:,gs_index]
         return (gs, gs_vec)
 
-def nevilleAlgo(i,j,z):
+def nevilleAlgo(i, j, z, excitation):
     if i == j:
-        return gs_eigensystem_r(9, (i + 4) * 5, 2, True)
+        return eigensystem_r(9, (i + 4) * 5, z, excitation, True)
     else:
         hi2 = (9 / (i + 4) / 5)**2
         hj2 = (9 / (j + 4) / 5)**2
-        return (nevilleAlgo(i, j - 1, z) * hj2 - hi2 * nevilleAlgo(i + 1, j, z)) / (hj2 - hi2)
+        return (nevilleAlgo(i, j - 1, z, excitation) * hj2 - hi2 * nevilleAlgo(i + 1, j, z, excitation)) / (hj2 - hi2)
 
 #print(nevilleAlgo(0, 4, 2))
 
-def graph(r, size, z):
+def graph(r, size, z, excitation):
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
 
-    _, eigenvector = gs_eigensystem_r(r, size, z)
+    gs, eigenvector = eigensystem_r(r, size, z, excitation)
 
     data = np.zeros((size, size))
     X = np.arange(1, size + 1)
@@ -84,11 +119,11 @@ def graph(r, size, z):
     for i in range(size**2):
         r1 = i // size
         r2 = i % size
-        data[r1, r2] = eigenvector[i]
+        data[r1, r2] = eigenvector[i]**2
 
     ax.plot_wireframe(X, Y, data)
     ax.view_init(-150, 225)
 
     plt.show()
 
-graph(9, 20, 2)
+#graph(9, 20, 2, 1)
